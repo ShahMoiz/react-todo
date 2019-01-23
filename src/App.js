cc1e6e70823a42e0ba2241829105e10531ccfe2c
shahmoiz <abdul_moiz10@hotmail.com>
2019-01-23 19:37:28 +0500

    Notification added, firebase deploy and bugs fix

--- src/App.js
+++ src/App.js
@@ -1,13 +1,28 @@
 import React, { Component } from 'react';
 import { MDBBtn } from "mdbreact";
-// import './App.css';
-import { Route, Link } from "react-router-dom";
+import {BrowserRouter as Router, Route, Link } from "react-router-dom";
+import NotificationAlert from 'react-notification-alert';
+import "react-notification-alert/dist/animate.css";
 import AddTodo from './AddTodo/addTodo.js'
 import Table from './Table/table.js';
 import Nav from './header/navbar/nav';
-// import Home from './Home/home.js';
-//Route Todos/:id functionaliy
+const baseUrl = process.env.PUBLIC_URL;
 var todosCompORIncomp = '';
+
+var options = {};
+function optionsFunc(msg, type){
+  return options = {
+    place: 'br',
+    message: (
+        <div>
+          <h4>{msg}</h4>
+        </div>
+    ),
+    type: type,
+    // icon: "now-ui-icons ui-1_bell-53",
+    autoDismiss: 3
+}
+}
 
 // Dummy Text for Todo Additional Info
 const addInfoTextDummy = 'Add Additional Info for Remembreing Task';
@@ -35,7 +50,8 @@ class App extends Component {
       todoAddInputValue: '',
       editTaskAfterAdded: { editButton: false, id: null },
       searchTodoValue: '',
-      todoAddInfoValue: ''
+      todoAddInfoValue: '',
+      getIsCompleteID: ''
 
     }
   }
@@ -50,14 +66,16 @@ class App extends Component {
     const todoObject = { todoName: this.state.todoAddInputValue, id: makeid(), isCompleted: false,
                      todoAddInfo: (this.state.todoAddInfoValue === '') ? addInfoTextDummy: this.state.todoAddInfoValue };
     this.state.todos.push(todoObject);
+    this.refs.notify.notificationAlert(optionsFunc("Added Successefully", 'success'));
     this.setState({ todos, todoAddInputValue: '' });
   }
 
   editTaskTable = (id) => {
     const getTask = this.state.todos.filter((todos) => todos.id === id);
     const editTaskAfterAdded = { editButton: true, id: id }
-
+    this.refs.notify.notificationAlert(optionsFunc("Edit Enable", 'info'));
     this.setState({ editTaskAfterAdded, todoAddInputValue: getTask[0].todoName });
+    
   }
 
   // this func is successefully complete edit this func is in AddTodo component  
@@ -65,7 +83,8 @@ class App extends Component {
     const editTaskAfterAdded = { editButton: false, id: null };
     const fetchID = this.state.editTaskAfterAdded.id;
     const findReqTodo = this.state.todos.filter((todos) => todos.id === fetchID);
-
+    
+    this.refs.notify.notificationAlert(optionsFunc("Edit Successfully", 'success'));
     findReqTodo.map(todo => { todo.todoName = this.state.todoAddInputValue; todo.todoAddInfo = this.state.todoAddInfoValue})
 
     this.setState({ todos, editTaskAfterAdded, todoAddInputValue: ''});
@@ -74,16 +93,20 @@ class App extends Component {
     const modiTodo = this.state.todos.filter((todo) => todo.id !== id);
     console.log("Delete Current Id,", modiTodo);
     // dltCurrentTodo.map((todo) => todo.)
+    this.refs.notify.notificationAlert(optionsFunc("Delete Successfully", 'danger'));
     this.setState({ todos: modiTodo })
+    
   }
   // Checkbox functioanlity this is in Table Component
-  isComplete = (isChecked, id) => {
-    const isCheckedVar = isChecked === false;
+  isComplete = (isChecked) => {
+    const id = this.state.getIsCompleteID;
+    console.log("Check isCHecked", isChecked)
     const getTodo = this.state.todos.filter((todo) => todo.id === id);
-
-    getTodo.map((todo) => todo.isCompleted = isCheckedVar);
+    console.log("IS Complete Get Todo", getTodo)
+    getTodo.map((todo) => todo.isCompleted = !todo.isCompleted);
 
     this.setState({ todos });
+    console.log("get Todos", this.state.todos)
   }
 
   isCompleteRouteTodo = () => {
@@ -104,13 +127,18 @@ class App extends Component {
     this.setState({todoAddInfoValue: e.target.value});
     console.log(this.state.todoAddInfoValue )
   }
+  getID = (id) => {
+    console.log("Get ID ", id)
+    this.setState({getIsCompleteID: id})
+  }
   render() {
     return (
+      <Router>
       <div className="App text-center">
         <Nav
           searchTodo={this.searchTodo}
         />
-        
+        <NotificationAlert ref="notify" />
         <Route path="/" exact render={() =>
           <div>
             <AddTodo
@@ -132,6 +160,7 @@ class App extends Component {
                   editTaskTable={this.editTaskTable}
                   dltTodo={this.dltTodo}
                   isComplete={this.isComplete}
+                  getID={this.getID}
                    />)
             }
           </div>
@@ -154,6 +183,7 @@ class App extends Component {
                   editTaskTable={this.editTaskTable}
                   dltTodo={this.dltTodo}
                   isComplete={this.isComplete}
+                  getID={this.getID}
                    />
               )
             }
@@ -191,6 +221,7 @@ class App extends Component {
           this.state.todoAddInputValue
         }
       </div>
+      </Router>
     );
   }
 }
--- src/index.js
+++ src/index.js
@@ -1,14 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router} from "react-router-dom";
+// import { BrowserRouter as Router} from "react-router-dom";
 import './index.css';
+
 import 'font-awesome/css/font-awesome.min.css';
 import 'bootstrap-css-only/css/bootstrap.min.css'; 
 import 'mdbreact/dist/css/mdb.css';
+// import "@fortawesome/fontawesome-free/css/all.min.css";
+// import 'bootstrap-css-only/css/bootstrap.min.css'; 
+// import 'mdbreact/dist/css/mdb.css';
+
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<Router><App /></Router>, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
